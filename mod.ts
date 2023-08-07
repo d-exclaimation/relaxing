f9f12dded1c68a5db22a80c3bff7c3f2ade25ba7
Vincent <thisoneis4business@gmail.com>
2023-08-08 00:30:30 +1200

    feat: Dash

--- mod.ts
+++ mod.ts
@@ -1,18 +1,41 @@
 import { router } from "https://deno.land/x/rutt@0.1.0/mod.ts";
 import { dashboard } from "./app/reviews/dash.ts";
+import { members } from "./app/team/members.ts";
 import "./lib/kv.ts";
 
 Deno.serve(
-  router(
-    {
-      "/": () => Response.json({ ok: true }),
-      "/dash": async () => {
-        const data = await dashboard();
+  router({
+    "/": () => Response.json({ ok: true }),
+    "/dash": async (req) => {
+      const url = new URL(req.url);
+      const id = url.searchParams.get("id");
+      const data = await dashboard();
+      if (!id) {
         return Response.json(data);
-      },
+      }
+      const dash = data.dash.find((d) => d.id === id);
+      if (!dash)
+        return Response.json(
+          { info: `Profile not found for 'id' of '${id}'` },
+          { status: 404 }
+        );
+      return Response.json({ dash, max: data.max });
     },
-    {
-      errorHandler: () => Response.json({ ok: false, error: "Not found" }),
-    }
-  )
+    "/profile": (req) => {
+      const url = new URL(req.url);
+      const id = url.searchParams.get("id");
+      if (!id)
+        return Response.json(
+          { info: `No 'id' query parameter provided` },
+          { status: 400 }
+        );
+      const profile = members.find((m) => m.id === id);
+      if (!profile)
+        return Response.json(
+          { info: `Profile not found for 'id' of '${id}'` },
+          { status: 404 }
+        );
+      return Response.json(profile);
+    },
+  })
 );
